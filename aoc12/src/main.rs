133be1a70f79ad9f4d2b52d2ef39d001ced66d53
Georg Reinke <guelfey@gmail.com>
2022-12-25 20:33:32 +0100

    day 12 part 2

--- aoc12/src/main.rs
+++ aoc12/src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 use std::collections::VecDeque;
 
+#[derive (Clone, Copy)]
 struct Node {
     elevation: i8,
     distance: isize,
@@ -36,7 +37,7 @@ fn neighbours(c: (usize, usize), f: &Vec<Vec<Node>>) -> Vec<(usize, usize)> {
 }
 
 fn main() {
-    let mut start = None;
+    let mut starts = Vec::new();
     let mut end = None;
     let mut field = Vec::new();
     loop {
@@ -64,8 +65,8 @@ fn main() {
                 distance: -1,
                 visited: false,
             });
-            if c == 'S' {
-                start = Some(coords);
+            if c == 'S' || c == 'a' {
+                starts.push(coords);
             }
             if c == 'E' {
                 end = Some(coords);
@@ -74,28 +75,37 @@ fn main() {
         field.push(field_line);
     }
 
-    let real_start = start.expect("should have found start");
     let real_end = end.expect("should have found end");
 
-    let mut queue : VecDeque<(usize, usize)> = VecDeque::new();
-    field[real_start.0][real_start.1].distance = 0;
-    queue.push_back(real_start);
-    while !queue.is_empty() {
-        let c = queue.pop_front().expect("queue should not be empty");
-        if field[c.0][c.1].visited {
-            continue;
-        }
-        dbg!(c);
-        field[c.0][c.1].visited = true;
-        let new_d = field[c.0][c.1].distance + 1;
-        for n in neighbours(c, &field) {
-            let d = field[n.0][n.1].distance;
-            if d == -1 || new_d < d {
-                field[n.0][n.1].distance = new_d;
+    let mut min_distance = std::isize::MAX;
+
+    for real_start in starts {
+        let mut this_field = field.to_vec();
+        let mut queue : VecDeque<(usize, usize)> = VecDeque::new();
+        this_field[real_start.0][real_start.1].distance = 0;
+        queue.push_back(real_start);
+        while !queue.is_empty() {
+            let c = queue.pop_front().expect("queue should not be empty");
+            if this_field[c.0][c.1].visited {
+                continue;
             }
-            queue.push_back(n);
+            this_field[c.0][c.1].visited = true;
+            let new_d = this_field[c.0][c.1].distance + 1;
+            for n in neighbours(c, &this_field) {
+                let d = this_field[n.0][n.1].distance;
+                if d == -1 || new_d < d {
+                    this_field[n.0][n.1].distance = new_d;
+                }
+                queue.push_back(n);
+            }
+        }
+
+        dbg!(real_start);
+        let distance = this_field[real_end.0][real_end.1].distance;
+        dbg!(distance);
+        if distance < min_distance && distance != -1 {
+            min_distance = distance;
         }
     }
-    println!("{} {}", real_end.0, real_end.1);
-    println!("{}", field[real_end.0][real_end.1].distance);
+    println!("{min_distance}");
 }
